afad91d2d08218cfaf2f6ab4258442d0f141a093
kacup <kacup@F_KACUPAJ>
2023-11-29 19:41:30 +0100

    Aggiungere i file di progetto.

---
+++ classe quadrato 1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classe_quadrato_1
+{
+    class Quadrato
+    {
+        private float lato;
+        public float Lato
+        {
+            get { return lato; }
+            set { lato = value; }
+        }
+        public Quadrato(float lato)
+        {
+            this.Lato = lato;
+        }
+    }
+    
+
+  
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float lLato;
+            Console.WriteLine("Inserisci la lunghezza del lato del quadrato:");
+            
+            if (float.TryParse(Console.ReadLine(), out lLato))
+            {
+                Quadrato quadrato = new Quadrato(lLato);
+                Console.WriteLine("il lato del quadrato è:{0}", quadrato.Lato);
+            }
+            else
+            {
+                Console.WriteLine("Inserimento non valido. Assicurati di inserire un numero valido per il lato.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
